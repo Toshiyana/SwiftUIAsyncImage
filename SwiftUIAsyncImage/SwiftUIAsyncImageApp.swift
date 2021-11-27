e5fe71204e80a8ef383ebada16c2aa0d56e20d23
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-27 14:24:52 +0900

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Toshiyana on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
