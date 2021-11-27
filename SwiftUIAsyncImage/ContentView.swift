e9ebc401ffd4883c08a5f6ad537d7aa7fe2bfef5
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-27 15:01:12 +0900

    add all

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -7,10 +7,77 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //MARK: - 1. Basic
+//        AsyncImage(url: URL(string: imageURL))
+        
+        //MARK: - 2. Scale
+        // The more big scale parameter is, the more small image size is.
+//        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        //MARK: - 3. Placeholder
+//        AsyncImage(url: URL(string: imageURL)) { image in
+//            image.imageModifier()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill").iconModifier()
+//        }
+//        .padding(40)
+        
+        //MARK: - Phase
+//        AsyncImage(url: URL(string: imageURL)) { phase in
+//            // SUCCESS: The image successfully loaded.
+//            // FAILURE: The image failed to load with an error.
+//            // EMPTY: No image is loaded.
+//
+//            if let image = phase.image {
+//                image.imageModifier()
+//            } else if phase.error != nil {
+//                Image(systemName: "ant.circle.fill").iconModifier()
+//            } else {
+//                Image(systemName: "photo.circle.fill").iconModifier()
+//            }
+//        }
+//        .padding(40)
+        
+
+        //MARK: - Animation
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .imageModifier()
+//                    .transition(.move(edge: .bottom)) // appear from bottom
+//                    .transition(.slide) // appear from left side
+                    .transition(.scale)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding(40)
     }
 }
 
